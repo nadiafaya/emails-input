dd18ee83ce8d3f6dd3514cb88cf2a04a3741e637
Nadia Faya <nadiafaya@gmail.com>
2020-06-15 00:31:41 +0200

    Dynamic html demo page

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -18,4 +19,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Demo page",
+    }),
+  ],
 };
