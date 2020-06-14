9d0f07142346f48f442027ace7615efdd266687b
Nadia Faya <nadiafaya@gmail.com>
2020-06-15 00:01:55 +0200

    Add imported styles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+const element = document.createElement("div");
+element.innerText = "Email Input";
+element.classList.add("email-input");
+
+document.body.appendChild(element);
