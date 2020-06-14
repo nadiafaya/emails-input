4d45dfbc7fb69e45d7755c8bc6568e156dcbc9bb
Nadia Faya <nadiafaya@gmail.com>
2020-06-15 00:07:37 +0200

    Add fonts

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["url-loader"],
+      },
     ],
   },
 };
