11ba222c81462d06c4ec5a54e9e5728c18fded4c
Nadia Faya <nadiafaya@gmail.com>
2020-06-16 23:03:53 +0200

    Rename index.html to demo.html

--- webpack.common.js
+++ webpack.common.js
@@ -30,6 +30,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Demo page',
+      filename: 'demo.html',
       inject: false,
       template: 'src/index.html',
     }),
